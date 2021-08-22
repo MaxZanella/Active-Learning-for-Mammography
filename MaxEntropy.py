7a6275fe8a81fdd279e670b6d60a2a86a4293561
MaxZanella <38530113+MaxZanella@users.noreply.github.com>
2021-08-22 22:15:10 +0200

    Uncertainty MaxEntropy

---
+++ MaxEntropy.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: maxim
+"""
+def query_uncertainty(model,unlabeled_loader,budget,device,measure="entropy"):
+    model.eval()
+    entropy_measure= np.zeros((len(unlabeled_loader)*model.params['batch_size'],))
+    with torch.no_grad():
+        i=0     
+        for batch_idx, (data, target) in enumerate(unlabeled_loader):
+            prob = np.zeros((data.shape[0],model.params['num_classes']))
+            data,target = data.to(device),target.to(device)
+            out = model(data)
+            out = F.softmax(out,dim=1)
+            out = out.cpu().numpy()
+            entropy_measure[i:i+data.shape[0]] = entropy(out).squeeze()
+            i = i + data.shape[0]
+    entropy_measure = entropy_measure[:i]
+    return np.argsort(entropy_measure)
